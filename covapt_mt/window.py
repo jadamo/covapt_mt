fixed number density calculation issue

covapt_mt/window.py
@@ -18,7 +18,7 @@ class Survey_Window_Kernels():
     """
     Class that handles both Super-Sample Covariance (SSC) window function and FFT 
     """
-    def __init__ (self, h:float, Om0:float, zbin, sample_bin, data_dir=covapt_data_dir):
+    def __init__ (self, h:float, Om0:float, zbin, sample_bin, data_dir=covapt_data_dir, random_file=""):
         """Constructs Survey_Window_Kernels object
 
         Args:
@@ -33,7 +33,7 @@ class Survey_Window_Kernels():
         """
 
         # load in random catalog
-        self.randoms = self.load_survey_randoms(zbin, sample_bin, data_dir)
+        self.randoms = self.load_survey_randoms(zbin, sample_bin, data_dir, random_file)
 
         # convert redshifts to physical distances based on some catalog cosmology
         self.convert_to_distances(h, Om0)
@@ -41,7 +41,7 @@ class Survey_Window_Kernels():
         # self.I22 = np.sum(self.randoms['NZ']**1 * self.randoms['WEIGHT_FKP']**2)
         # self.I22 = self.I22.compute()    
 
-    def load_survey_randoms(self, zbin, sample_bin, data_dir):
+    def load_survey_randoms(self, zbin, sample_bin, data_dir, file_name=""):
         """Loads random survey catalog from file and
         
         Args:
@@ -52,7 +52,10 @@ class Survey_Window_Kernels():
             IOError: If random catalog doesn't exist in the specified directory
 
         """
-        random_file = "random_sample_"+str(sample_bin)+"_redshift_"+str(zbin)+".fits"
+        if file_name == "":
+            random_file = "random_sample_"+str(sample_bin)+"_redshift_"+str(zbin)+".fits"
+        else:
+            random_file = file_name
 
         if not os.path.exists(data_dir+random_file):
             raise IOError("Could not find survey randoms catalog:", data_dir+random_file)

scripts/make_window_function.py
@@ -9,24 +9,26 @@ from multiprocessing import Pool
 from covapt_mt import window
 from covapt_mt.config import covapt_data_dir
 
-calc_FFTs = False
+calc_FFTs = True
 calc_SSC_window = False
 calc_Gaussian_window = True
 
 # ints specifying which redshift and sample bins to generate windows for
 # there are 5 sample bins and 11 redshift bins
-zbin = 2
-sample_bin = 2
+zbin = 1
+sample_bin = 1
 
 # Location of random catalogs
-data_dir = "/home/joeadamo/Research/SPHEREx/covapt_mt/data/"
+data_dir = "/Users/JoeyA/Research/SPHEREx/covapt_mt/data/"
+
+random_file = "random_for_Robin.fits"
 
 # how many processers to use for the Gaussian window kernels
 num_processes = 14
 
 # number of kmodes to sample
 # The default used by Jay Wadekar was 25000, which was run on a cluster
-kmodes_sampled = 25000
+kmodes_sampled = 10 #25000
 
 # K bins to generate the Gaussian window function for
 k_centers = np.linspace(0.01, 0.25, 25)
@@ -39,7 +41,7 @@ def main():
     print("Calculating Gaussian window functions:", calc_Gaussian_window)
 
     if calc_FFTs or calc_SSC_window:
-        survey_kernels = window.Survey_Window_Kernels(0.7, 0.31, zbin, sample_bin, data_dir)
+        survey_kernels = window.Survey_Window_Kernels(0.7, 0.31, zbin, sample_bin, data_dir, random_file)
 
     if calc_FFTs:
         print("\nStarting FFT calculations...")
@@ -78,7 +80,8 @@ def main():
         t2 = time.time()
         print('Done! Run time: {:.0f}m {:.0f}s'.format((t2-t1) // 60, (t2-t1) % 60))
 
-        save_file = covapt_data_dir+'Wij_k'+str(nBins)+'_sample_'+str(sample_bin)+'_redshift_'+str(zbin)+'.npy'
+        #save_file = covapt_data_dir+'Wij_k'+str(nBins)+'_sample_'+str(sample_bin)+'_redshift_'+str(zbin)+'.npy'
+        save_file = covapt_data_dir+"Wij_k"+str(nBins)+"_for_Robin.npy"
         b=np.zeros((len(idx),7,15,6))
         for i in range(len(idx)):
             b[i]=WinFunAll[i]